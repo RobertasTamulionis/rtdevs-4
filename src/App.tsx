a1f724db21e49a7eb538368c7cf8e65a30867139
RobertasTamulionis <rtdevs@protonmail.com>
2025-08-31 10:04:07 +0200

    adds Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { Analytics } from "@vercel/analytics/react";
 import Sidebar from './components/Sidebar/Sidebar';
 import Content from './components/Content/Content';
 import NavMobile from './components/NavMobile/NavMobile';
@@ -13,6 +14,7 @@ function App() {
           <NavMobile/>
           <Sidebar/>
           <Content/>
+          <Analytics />
         </div>
     </Provider>
   )
